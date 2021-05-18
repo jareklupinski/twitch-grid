d4c28d8602c45a742b262a0272cccc645f521c2d
Jarek Lupinski <jarek@lupin.ski>
2021-05-17 23:41:35 -0400

    json stuff

--- app/views.py
+++ app/views.py
@@ -1,9 +1,17 @@
+import json
+
 from django.shortcuts import render
 
 from app.models import Game
 
 
 def index(request):
-    game_list = Game.objects.all()[:10]
+    game_list = []
+    games = Game.objects.all()[:10]
+    for game in games:
+        game_list.append({
+            "name": game.name,
+            "streamers": json.loads(game.streamers)
+        })
     context = {'game_list': game_list}
     return render(request, 'app/index.html', context)
