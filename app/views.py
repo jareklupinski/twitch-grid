1360496ab095fd7d5eeb9b70997cbb6f017333e9
Jarek Lupinski <jarek@lupin.ski>
2021-05-18 00:15:00 -0400

    pagination

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from app.models import Game
 
 def index(request):
     games = Game.objects.all()
-    paginator = Paginator(games, 10)
+    paginator = Paginator(games, 100)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
