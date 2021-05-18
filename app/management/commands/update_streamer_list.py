fc1f5b230c53038c3ad8c72df719b5f0fc3aa619
Jarek Lupinski <jarek@lupin.ski>
2021-05-18 12:47:41 -0400

    async

--- app/management/commands/update_streamer_list.py
+++ app/management/commands/update_streamer_list.py
@@ -76,7 +76,7 @@ async def async_get_twitch_api_data(url: str, token: str, session, game_id=None,
             "game_id": game_id
         }
         async with session.get(url=url, headers=headers, params=params) as response:
-            response_data = await response.json()
+            response_data = await json.loads(response.text)
         rate_limit_remaining_string = response.headers.get("Ratelimit-Remaining")
         if rate_limit_remaining_string is not None:
             rate_limit_remaining = int(rate_limit_remaining_string)
