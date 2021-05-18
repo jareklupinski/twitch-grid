f8400d829bded18aa3cb078405a7c051741920cb
Jarek Lupinski <jarek@lupin.ski>
2021-05-17 23:25:52 -0400

    print debugging

--- app/views.py
+++ app/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 from app.models import Game
 
+
 def index(request):
+    print("got request")
     game_list = Game.objects.all()
+    print("got games")
     context = {'game_list': game_list}
+    print("got context")
     return render(request, 'index.html', context)
