a519cae981a8c1a84aee642205cac9fcf79bb374
Jarek Lupinski <jarek@lupin.ski>
2021-05-17 21:49:16 -0400

    chonky ints

--- app/models.py
+++ app/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Streamer(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.BigIntegerField(primary_key=True)
     url = models.URLField()
     viewer_count = models.IntegerField()
 
 
 class Game(models.Model):
-    id = models.CharField(max_length=255, primary_key=True)
+    id = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     streamers = models.ManyToManyField('Streamer', related_name='games')
