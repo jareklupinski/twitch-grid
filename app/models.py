437c34ee872fb996de3e2e91cc17b58da504c0e5
Jarek Lupinski <jarek@lupin.ski>
2021-05-17 20:56:58 -0400

    initial commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Streamer(models.Model):
+    viewer_count = models.IntegerField()
+    url = models.TextField()
+
+
+class Game(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+    streamers = models.ManyToManyField(Streamer)
