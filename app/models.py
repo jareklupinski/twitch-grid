4f046aad7f7c7f0fe3068858af4ab375e1889b0e
Jarek Lupinski <jarek@lupin.ski>
2021-05-18 01:58:00 -0400

    longer urls

--- app/models.py
+++ app/models.py
@@ -14,8 +14,8 @@ class Process(models.Model):
 
 class Game(models.Model):
     id = models.BigIntegerField(primary_key=True)
-    name = models.CharField(max_length=255)
-    box_art_url = models.URLField()
+    name = models.CharField(max_length=1024)
+    box_art_url = models.CharField(max_length=1024)
     streamers = models.JSONField()
     total_viewers = models.IntegerField()
     # streamers = models.ManyToManyField('Streamer', related_name='games')
