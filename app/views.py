7eaf43013d64eab3b3033dcb8bc6c983cbf48f1c
Jarek Lupinski <jarek@lupin.ski>
2021-05-17 23:56:49 -0400

    pagination

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
-import json
-
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from app.models import Game
@@ -8,10 +7,8 @@ from app.models import Game
 def index(request):
     game_list = []
     games = Game.objects.all()
-    for game in games:
-        game_list.append({
-            "name": game.name,
-            "streamers": game.streamers
-        })
-    context = {'game_list': game_list}
+    paginator = Paginator(games, 100)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'app/index.html', context)
