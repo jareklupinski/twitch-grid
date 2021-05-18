fe652ecaf70b4eece5e2410a7083dc67563a9060
Jarek Lupinski <jarek@lupin.ski>
2021-05-18 01:39:32 -0400

    style text

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from app.models import Game, Process
 def index(request):
     games = Game.objects.all().order_by("-total_viewers")
     process = Process.objects.get(name="game_list_update")
-    paginator = Paginator(games, 500)
+    paginator = Paginator(games, 200)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
