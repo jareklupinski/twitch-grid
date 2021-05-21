074c88d42ffeba7dfd0f72480e586020ee04ffe7
Jarek Lupinski <jarek@lupin.ski>
2021-05-21 17:28:40 -0400

    add memcachier layer

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
 from app.models import Game, Process
 
+timeout = 300
 
+
+@cache_page(timeout)
 def index(request):
     games = Game.objects.all().order_by("-total_viewers")
     process = Process.objects.get(name="game_list_update")
