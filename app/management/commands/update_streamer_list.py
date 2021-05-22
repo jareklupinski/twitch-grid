5b2118b53932b08b037d2c3806b78cb2fc0c2efb
Jarek Lupinski <jarek@lupin.ski>
2021-05-22 19:06:20 -0400

    magic number too big

--- app/management/commands/update_streamer_list.py
+++ app/management/commands/update_streamer_list.py
@@ -103,7 +103,7 @@ async def get_streamer_list(game, session, twitch_oauth_token):
         magic_number_viewers += viewer_count ** 3
 
     # fiddling with the magic number calculation
-    magic_number = magic_number_viewers / len(streamers)
+    magic_number = (magic_number_viewers / len(streamers)) ** (1. / 3)
 
     new_game, _ = await sync_to_async(Game.objects.update_or_create)(
         id=game_id,
